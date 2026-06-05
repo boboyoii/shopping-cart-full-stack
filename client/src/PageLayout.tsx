489467371dfde39a63fae68ec7008e86dabff9bb
boyeon <boyeon6331@gmail.com>
2026-06-05 15:29:09 +0900

    feat: 장바구니 페이지 기본 레이아웃 구현

---
+++ client/src/PageLayout.tsx
@@ -0,0 +1,44 @@
+import type { ReactNode } from 'react';
+import styled from '@emotion/styled';
+
+interface PageLayoutProps {
+  headerContent: ReactNode;
+  children: ReactNode;
+}
+
+const PageLayout = ({ headerContent, children }: PageLayoutProps) => {
+  return (
+    <PageWrapper>
+      <Header>
+        <HeaderContent>{headerContent}</HeaderContent>
+      </Header>
+
+      <Main>{children}</Main>
+    </PageWrapper>
+  );
+};
+
+const PageWrapper = styled.div`
+  max-width: 26rem;
+  min-height: 100vh;
+  margin-inline: auto;
+`;
+
+const Header = styled.header`
+  height: 4rem;
+  background-color: #000000;
+`;
+
+const HeaderContent = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding-inline: 1rem;
+`;
+
+const Main = styled.main`
+  min-height: calc(100vh - 4rem);
+  padding: 2.25rem 1.5rem;
+`;
+
+export default PageLayout;
