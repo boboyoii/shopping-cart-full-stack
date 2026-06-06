641fb7d4b312bb101baa4181dcd5603d24110ef1
boyeon <boyeon6331@gmail.com>
2026-06-06 10:45:13 +0900

    feat: 주문 확인 페이지 컴포넌트 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
-import CartItemListPage from './CartItemListPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CartItemListPage from './CartPage';
+import OrderConfirmPage from './OrderConfirmPage';
 
 function App() {
-  return <CartItemListPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CartItemListPage />} />
+        <Route path="/order-confirm" element={<OrderConfirmPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
